73fcac9053540cf26ebf4e329ae2ff6621c7dee0
unknown <paatatepnadze1@gmail.com>
2022-10-19 15:14:28 +0200

    Formula creation rounded assesgment

--- TestApp/TestApp/Program.cs
+++ TestApp/TestApp/Program.cs
@@ -1,8 +1,28 @@
-﻿class Sample
+﻿class Formula
 {
     static void Main()
     {
-        System.Console.WriteLine("Hallo");
+
+        int inputNumber = 3852;  //input
+        
+
+        decimal decimaInputNumber = inputNumber;
+
+        
+
+        decimal lastDigits = decimaInputNumber / 1000;
+        decimal firstDigits = inputNumber / 1000;
+        decimal compareValue = (lastDigits - firstDigits) * 1000;
+
+
+        int thouseequalntCounter = inputNumber / 1000 * 1000;
+        int moreOrEqualFhundred = (compareValue >= 500) ? 1000 : 0 ;
+        int lessThanFhundred = (inputNumber < 500) ? 1000 : 0 ;
+
+        int finalRoundedResult = thouseequalntCounter + moreOrEqualFhundred + lessThanFhundred;
+
+        System.Console.WriteLine(finalRoundedResult);
         System.Console.ReadKey();
+    
     }
 }
