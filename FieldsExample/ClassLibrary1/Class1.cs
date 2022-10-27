8b300493b11e7bfa1388a180bf77cccfd161a350
unknown <paatatepnadze1@gmail.com>
2022-10-27 11:27:33 +0200

    Encapsylation get/swet method cration

--- FieldsExample/ClassLibrary1/Class1.cs
+++ FieldsExample/ClassLibrary1/Class1.cs
@@ -20,6 +20,84 @@ public class Product
         dateOfPurchase = System.DateTime.Now.ToShortDateString();
     }
 
+    //set method for productID
+    public void SetProductId(int value)
+    {
+        productID = value;
+    }
+
+    //get method for productID
+    public int GetProductID()
+    {
+        return productID;
+    }
+    //set method for productName
+    public void SetProductName(string value)
+    {
+        productName = value;
+    }
+
+    //get method for productID
+    public string GetProductName()
+    {
+        return productName;
+    }
+
+     //setmethod for cost
+    public void SetCost(double value)
+    {
+        cost = value;
+    }
+
+    //get method for cost
+    public double GetCost()
+    {
+        return cost;
+    }
+   
+    //setmethod for tax
+    public void SetTax(double value)
+    {
+        tax = value;
+    }
+
+    //get method for tax
+    public double GetTax()
+    {
+        return tax;
+    }
+    //setmethod for quantityStock
+    public void SetQuantityStock(int value)
+    {
+        quantityStock = value;
+    }
+
+    //get method for quantityStock
+    public int GetQuantityStock()
+    {
+        return quantityStock;
+    }
+
+    //get method for dateOfPurchase
+    public string GetDateOfPurchase()
+    {
+        return dateOfPurchase;
+    }
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     public void CalculateTax() 
     {
          double t;
--- FieldsExample/FieldsExample/Program.cs
+++ FieldsExample/FieldsExample/Program.cs
@@ -23,20 +23,20 @@ class Sample
 
 
         //initialize fields
-        product1.productID = 1001;
-        product1.productName = "Mobile";
-        product1.cost = 200;
-        product1.quantityStock = 1200;
+        product1.SetProductId(1001);
+        product1.SetProductName("Mobile");
+        product1.SetCost(200);
+        product1.SetQuantityStock(1200);
 
-        product2.productID = 1002;
-        product2.productName = "Laptop";
-        product2.cost = 4500;
-        product2.quantityStock = 3400;
+        product2.SetProductId(1002);
+        product2.SetProductName("Laptop");
+        product2.SetCost(4500);
+        product2.SetQuantityStock(3400);
 
-        product3.productID = 1003;
-        product3.productName = "speaker";
-        product3.cost = 36000;
-        product3.quantityStock = 800;
+        product3.SetProductId(1003);
+        product3.SetProductName("speaker");
+        product3.SetCost(36000);
+        product3.SetQuantityStock(800);
 
         //call methods
         product1.CalculateTax();
@@ -49,30 +49,31 @@ class Sample
         System.Console.WriteLine(developerName);
 
         System.Console.WriteLine("Product1: ");
-        System.Console.WriteLine("Product ID: " + product1.productID);
-        System.Console.WriteLine("Product Name: " + product1.productName);
-        System.Console.WriteLine("Cost: " + product1.cost);
-        System.Console.WriteLine("Quantity  in Stock: " + product1.quantityStock);
-        System.Console.WriteLine("Date of purchasek: " + product1.dateOfPurchase);
-        System.Console.WriteLine("Tax: " + product1.tax);
+        System.Console.WriteLine("Product ID: " + product1.GetProductID());
+        System.Console.WriteLine("Product Name: " + product1.GetProductName());
+        System.Console.WriteLine("Cost: " + product1.GetCost());
+        System.Console.WriteLine("Quantity  in Stock: " + product1.GetQuantityStock());
+        System.Console.WriteLine("Date of purchasek: " + product1.GetDateOfPurchase());
+        System.Console.WriteLine("Tax: " + product1.GetTax() );
 
         System.Console.WriteLine("\nProduct2: ");
-        System.Console.WriteLine("Product ID: " + product2.productID);
-        System.Console.WriteLine("Product Name: " + product2.productName);
-        System.Console.WriteLine("Cost: " + product2.cost);
-        System.Console.WriteLine("Quantity  in Stock: " + product2.quantityStock);
-        System.Console.WriteLine("Date of purchasek: " + product2.dateOfPurchase);
-        System.Console.WriteLine("Tax: " + product2.tax);
+        System.Console.WriteLine("Product ID: " + product2.GetProductID());
+        System.Console.WriteLine("Product Name: " + product2.GetProductName());
+        System.Console.WriteLine("Cost: " + product2.GetCost());
+        System.Console.WriteLine("Quantity  in Stock: " + product2.GetQuantityStock());
+        System.Console.WriteLine("Date of purchasek: " + product2.GetDateOfPurchase());
+        System.Console.WriteLine("Tax: " + product2.GetTax());
 
         System.Console.WriteLine("\nProduct3: ");
-        System.Console.WriteLine("Product ID: " + product3.productID);
-        System.Console.WriteLine("Product Name: " + product3.productName);
-        System.Console.WriteLine("Cost: " + product3.cost);
-        System.Console.WriteLine("Quantity  in Stock: " + product3.quantityStock);
-        System.Console.WriteLine("Date of purchasek: " + product3.dateOfPurchase);
-        System.Console.WriteLine("Tax: " + product3.tax);
+        System.Console.WriteLine("Product ID: " + product3.GetProductID());
+        System.Console.WriteLine("Product Name: " + product3.GetProductName());
+        System.Console.WriteLine("Cost: " + product3.GetCost());
+        System.Console.WriteLine("Quantity  in Stock: " + product3.GetQuantityStock());
+        System.Console.WriteLine("Date of purchasek: " + product3.GetDateOfPurchase());
+        System.Console.WriteLine("Tax: " + product3.GetTax());
 
-        int totalQuantitty = product1.quantityStock + product2.quantityStock + product3.quantityStock;
+        //total quantity
+        int totalQuantitty = product1.GetQuantityStock() + product2.GetQuantityStock() + product3.GetQuantityStock();
 
         System.Console.WriteLine("---------------------------------------------");
         System.Console.WriteLine("Total Quantity" + totalQuantitty);
